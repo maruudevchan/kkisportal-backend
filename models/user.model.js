89db20bd54d60409d780baee3cd8a0c8edf89403
Marie Azonipse <maruudevchan@gmail.com>
2023-07-20 02:53:24 -0500

    added userModel for authModule

---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+import { Model, DataTypes } from "sequelize";
+import {DatabaseConfig} from "../config/database.js";
+
+export class UserModel extends Model {}
+
+UserModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    pwd: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isOnline : {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    
+},
+    {
+        sequelize: DatabaseConfig,
+        tableName: 'users',
+        timestamps: false
+    }
+);
+
+// {
+//     "username":"maruudevchan",
+//     "password":"1234",
+//     "name":"Gloria Espinoza",
+//     
+// }
